43521fda6f4bd64fd3271d87117ce14cf36079e2
clemens.eyhoff <clemens.eyhoff@fit.fraunhofer.de>
2023-01-04 16:18:28 +0100

    fixed rtc connection

--- wordclock/src/time.cpp
+++ wordclock/src/time.cpp
@@ -12,11 +12,23 @@ void Time::setup() {
 
   if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
-    delay(1000);
-  } else if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC (2nd try)");
+    // Serial.flush();
+    // for (int i = 0; i< 1000;i++){
+    //   delay(10);
+    // }
+  } 
+  
+   if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
+  Time:ntpClient.setUpdateInterval(600000);
   Time::ntpClient.begin();
   Time::ntpClient.update();
 }
@@ -27,11 +39,19 @@ void Time::loop() {
   int m = now.minute();
 
   if (Time::ntpClient.update()) {
+    if (! rtc.isrunning()) {
+      if (!rtc.begin()) {
+         Serial.println("Couldn't find RTC");
+      } 
+    }
+    if (Time::ntpClient.isTimeSet()){
+      rtc.adjust(DateTime(Time::ntpClient.getEpochTime()));
+    }
+  }
+  if (Time::ntpClient.isTimeSet()){
     h = Time::ntpClient.getHours();
     m = Time::ntpClient.getMinutes();
-    rtc.adjust(DateTime(Time::ntpClient.getEpochTime()));
   }
-  Time::ntpClient.update();
 
   if(m != Time::minute) {
     if(m == 0 && h == Time::hour) {
@@ -40,6 +60,11 @@ void Time::loop() {
     Time::hour = h;
     Time::minute = m;
     Grid::setTime(Time::hour, Time::minute);
+
+    // char buf[15];
+    // sprintf(buf, "Time: %02d:%02d", Time::hour, Time::minute);
+    // Serial.println(buf);
+    Serial.println(Time::ntpClient.getFormattedTime());
 
     if (Config::automatic_timezone) {
       UtcOffset::updateLocalizedUtcOffset();
