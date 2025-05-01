d2f794c60593c855c81782e6bd24fe02e828ab9a
Clemens Eyhoff <clemens.eyhoff@fit.fraunhofer.de>
2025-05-01 10:22:26 +0200

    ♻️ (utcOffset.cpp): replace deprecated API calls with modern alternatives; refactor UTC offset parsing to improve readability and accuracy

--- wordclock/src/utcOffset.cpp
+++ wordclock/src/utcOffset.cpp
@@ -4,6 +4,13 @@
 #include "utcOffset.h"
 #include "config.h"
 
+int parseUtcOffsetToHours(const String& utcOffset) {
+  int sign = (utcOffset[0] == '-') ? -1 : 1;
+  int hours = utcOffset.substring(1, 3).toInt();
+  
+  return sign * hours;
+}
+
 void UtcOffset::updateLocalizedUtcOffset() {
 /* remove the depricated ESP8266 API call:  
  *HTTPClient http;
@@ -13,7 +20,8 @@ void UtcOffset::updateLocalizedUtcOffset() {
 //  use the new API like below:
   WiFiClient client;
   HTTPClient http;
-  http.begin(client, "http://worldtimeapi.org/api/ip");
+  //http.begin(client, "http://worldtimeapi.org/api/ip");
+  http.begin(client, "https://ipapi.co/json");
   int responseCode = http.GET();
 
   if (responseCode == 200) {
@@ -22,13 +30,16 @@ void UtcOffset::updateLocalizedUtcOffset() {
     StaticJsonDocument<1024> doc;
     deserializeJson(doc, payload);
 
-    int utcOffset = doc["raw_offset"].as<int>();
-    int dstOffset = doc["dst_offset"].as<int>();
+    // int utcOffset = doc["raw_offset"].as<int>();
+    // int dstOffset = doc["dst_offset"].as<int>();
+
+    // const int newTimezone = utcOffset + dstOffset;
+    String utcOffsetStr = doc["utc_offset"];
+    const int newTimezone = parseUtcOffsetToHours(utcOffsetStr);
 
     http.end();
 
     const int oldTimezone = Config::timezone;
-    const int newTimezone = utcOffset + dstOffset;
 
     if (oldTimezone != newTimezone) {
 
@@ -36,9 +47,9 @@ void UtcOffset::updateLocalizedUtcOffset() {
       Serial.print("Old timezone: ");
       Serial.println(Config::timezone);
       Serial.print("New timezone: ");
-      Serial.println(utcOffset + dstOffset);
+      Serial.println(newTimezone);
 
-      Config::timezone = utcOffset + dstOffset;
+      Config::timezone = newTimezone;
       Config::save();
     }
 
